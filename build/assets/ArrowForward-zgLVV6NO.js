09ebdf83c9f13c816f32d6daa3bf42dc31fc3c3e
prakash282 <dev@abmediaco.com>
2026-01-05 18:35:36 +0530

    add

---
+++ build/assets/ArrowForward-zgLVV6NO.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-B5yy5ya_.js";import{c as r}from"./createSvgIcon-Bq_VmRVw.js";const a=r(o.jsx("path",{d:"m12 4-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"}));export{a as A};
